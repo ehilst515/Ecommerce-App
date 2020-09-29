3adfd0c31d815c85d7ed3594644b02c01425538f
Keith Dahlby <keith@newbo.co>
2020-09-29 09:58:04 -0500

    Switch some strings to numbers

--- WebApplication2/Models/Cereal.cs
+++ WebApplication2/Models/Cereal.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ECommerceApp.Models
+﻿namespace ECommerceApp.Models
 {
     public class Cereal
     {
         public string Name { get; set; }
         public string Mfr { get; set; }
         public string Type { get; set; }
-        public string Calories { get; set; }
+        public int Calories { get; set; }
         public string Protein { get; set; }
         public string Fat { get; set; }
         public string Sodium { get; set; }
@@ -20,8 +15,8 @@ namespace ECommerceApp.Models
         public string Potass { get; set; }
         public string Vitamins { get; set; }
         public string Shelf { get; set; }
-        public string Weight { get; set; }
-        public string Cups { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Cups { get; set; }
         public string Rating { get; set; }
     }
 }
--- WebApplication2/Services/CerealRepository.cs
+++ WebApplication2/Services/CerealRepository.cs
@@ -24,7 +24,7 @@ namespace ECommerceApp.Services
                     Name = column[0],
                     Mfr = column[1],
                     Type = column[2],
-                    Calories = column[3],
+                    Calories = Convert.ToInt32(column[3]),
                     Protein = column[4],
                     Fat = column[5],
                     Sodium = column[6],
@@ -34,8 +34,8 @@ namespace ECommerceApp.Services
                     Potass = column[10],
                     Vitamins = column[11],
                     Shelf = column[12],
-                    Weight = column[13],
-                    Cups = column[14],
+                    Weight = decimal.Parse(column[13]),
+                    Cups = decimal.Parse(column[14]),
                     Rating = column[15],
                 });
             }
