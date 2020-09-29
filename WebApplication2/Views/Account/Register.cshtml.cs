e8bb7bcbcd50000edbd10d654452e784912a9d43
Jennifer B Jones <66266330+jenjones319@users.noreply.github.com>
2020-09-29 17:37:49 -0500

    added layout

---
+++ WebApplication2/Views/Account/Register.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Views.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            return Page();
+        }
+
+        [BindProperty]
+        public RegisterInput Input { get; set; }
+
+        public class RegisterInput
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm Password")]
+            [Compare(nameof(Password))]
+            public string ConfirmPassword { get; set; }
+        }
+
+    }
+}
